016e70f9a3f51f2792789ea7816d275c1783c5c6
Miguel <miguelmds925@gmail.com>
2023-10-21 17:43:09 -0300

    Refatorando o código

--- ciclic/assets/js/main.js
+++ ciclic/assets/js/main.js
@@ -1,29 +1,39 @@
 const form = document.querySelector(".form")
 
+const firstName = document.getElementById("name")
+const monthlyPayment = document.getElementById("monthlyPayment")
+const time = document.getElementById("time")
+const value = document.querySelector(".value")
+const reset = document.querySelector(".reset")
+const infos = document.querySelector(".infos")
+
+function calcForm() {
+  const expr = (monthlyPayment.value * (((1 + 0.00517) ^ time.value - 1) / 0.00517)).toFixed(2)
+
+  return expr
+}
+
+function addInfos() {
+  infos.classList.remove("hidden")
+
+  value.innerHTML = `Olá ${firstName.value}, juntando R$${monthlyPayment.value} você terá R$${calcForm()} em ${time.value} meses`
+}
+
+function resetForm() {
+  firstName.value = ""
+  monthlyPayment.value = ""
+  time.value = "" 
+  infos.classList.add("hidden")
+}
+
 form.addEventListener("submit", (event) =>{
   event.preventDefault()
 
-  const name = document.getElementById("name").value
-  const monthlyPayment = document.getElementById("monthlyPayment").value
-  const time = document.getElementById("time").value
+  calcForm()
+  addInfos()
+})
 
-  const expr = (monthlyPayment * (((1 + 0.00517) ^ time - 1) / 0.00517)).toFixed(2)
-
-  const value = document.querySelector(".value")
-  document.querySelector(".infos").classList.remove("hidden")
-
-  value.textContent = `Olá ${name}, juntando R$${monthlyPayment} você terá R$${expr} em ${time} meses`
-
-  const reset = document.querySelector(".reset")
-
-  reset.addEventListener("click", (event) => {
-    
-
-    name.textContent = ""
-    monthlyPayment.textContent = ""
-    time.textContent = "" 
-
-    console.log("funcionando")
-  })
+reset.addEventListener("click", (event) => {
+  resetForm()
 })
 
